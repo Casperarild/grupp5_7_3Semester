d800ea95c49f80999a615ea794d72c83970923e5
Casper Arild <casperarildp@gmail.com>
2025-11-29 23:27:59 +0100

    Test2

--- olimex/olimexAirconUnit/html/navtreedata.js
+++ olimex/olimexAirconUnit/html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "Optilogic Aircon Monitoring", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
